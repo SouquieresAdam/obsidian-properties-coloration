06c2f58e26d67ff1d250cb32092490c9e11948f5
Adam Souquières <souquieres.adam@gmail.com>
2023-04-02 14:45:45 +0200

    Version 1.0.0

--- main.ts
+++ main.ts
@@ -1,81 +1,59 @@
 import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface PropertyColorationSettings {
+	keyColor: string;
+	valueColor: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: PropertyColorationSettings = {
+	keyColor: 'default',
+	valueColor: 'default'
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class PropertyColorationPlugin extends Plugin {
+	settings: PropertyColorationSettings;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+	    this.registerMarkdownCodeBlockProcessor("properties", (source, el, ctx) => {
+			const rows = source.split("\n").filter((row) => row.length > 0);
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+			for (let i = 0; i < rows.length; i++) {
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+			  const propDiv = el.createDiv();
+			 
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
+
+
+              if(rows[i].startsWith("#")) {
+				propDiv.style.color="grey"
+				propDiv.textContent = rows[i];
+				continue; 
+			  }
+
+			  const cols = rows[i].split("=");
+
+			  const id = cols[0]
+			  const value = cols[1]
+
+			  const keySpan = propDiv.createSpan();
+			  const separatorSpan = propDiv.createSpan();
+			  const valueSpan = propDiv.createSpan();
+
+			  keySpan.textContent = id;
+			  keySpan.style.color = this.settings.keyColor;
+
+			  separatorSpan.textContent = "=";
+
+			  valueSpan.textContent = value;
+			  valueSpan.style.color = this.settings.valueColor;
 			}
-		});
+		  });
+
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.addSettingTab(new PropertiesColorationSettingTab(this.app, this));
 	}
 
 	onunload() {
@@ -107,10 +85,10 @@ class SampleModal extends Modal {
 	}
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+class PropertiesColorationSettingTab extends PluginSettingTab {
+	plugin: PropertyColorationPlugin;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: PropertyColorationPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
@@ -120,17 +98,29 @@ class SampleSettingTab extends PluginSettingTab {
 
 		containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+		containerEl.createEl('h2', {text: 'Property Coloration Setting'});
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('Key Color')
+			.setDesc('html color code for property key coloration')
 			.addText(text => text
 				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setValue(this.plugin.settings.keyColor)
 				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
+					console.log('New key Color: ' + value);
+					this.plugin.settings.keyColor = value;
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+			.setName('Value Color')
+			.setDesc('html color code for property key coloration')
+			.addText(text => text
+				.setPlaceholder('Enter your secret')
+				.setValue(this.plugin.settings.valueColor)
+				.onChange(async (value) => {
+					console.log('New value Color: ' + value);
+					this.plugin.settings.valueColor = value;
 					await this.plugin.saveSettings();
 				}));
 	}
